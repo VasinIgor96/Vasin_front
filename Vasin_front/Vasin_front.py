ec039b57e982bf0806a35a0d17a6864a3d0f73f5
Vasin Igor <vasin.igor0796@gmail.net>
2023-05-21 18:03:37 +0300

    save

--- Vasin_front/Vasin_front.py
+++ Vasin_front/Vasin_front.py
@@ -1,1 +1,19 @@
 
+import flet as ft
+
+from home import Home
+
+
+async def main(page: ft.page):
+    page.window_min_width = 350
+    page.window_min_height = 750
+    page.title = "ProSkill"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.theme = ft.Theme(color_scheme_seed="indigo")
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.scroll = "auto"
+    await page.add_async(Home())
+
+
+
+ft.app(target=main)
