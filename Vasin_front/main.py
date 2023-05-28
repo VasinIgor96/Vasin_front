eebe9ad490a2b13a4d244ce0b93f52e8d6d3b556
Vasin Igor <vasin.igor0796@gmail.net>
2023-05-28 20:35:21 +0300

    save 28.05

--- Vasin_front/main.py
+++ Vasin_front/main.py
@@ -5,101 +5,131 @@ from home import Home
 
 
 def main(page: ft.page):
-    page.window_min_width = 350
+    page.window_min_width = 400
     page.window_min_height = 750
     page.title = "ProSkill"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.theme = ft.Theme(color_scheme_seed="indigo")
     page.theme_mode = ft.ThemeMode.LIGHT
-    #page.scroll = "auto"
-    
+    # page.scroll = "auto"
+
     def route_change(route):
         page.views.clear()
         page.views.append(
             ft.View(
                 "/",
                 [
-                   ft.Stack(
-            [
-                ft.Row(
-                    [
-                    ft.Image(
-                        f"https://github.com/VasinIgor96/New-Project/blob/master/Fonnn.jpg?raw=true", 
-                        fit=ft.ImageFit.FILL
-                    ),
-                    ],
-                  
-                  ),
-                 ft.Container(
-                    margin = ft.margin.only(top = 175),
-                    alignment=ft.alignment.center,
-                    content=ft.Card(
-                        content=ft.Container(
-                            
-                            height=400,
-                            width=400,
-                            padding=25,
-                            content=ft.Column(
+                    ft.Stack(
+                        [
+                            ft.Row(
                                 [
-                                    ft.Row(
-                                        [ft.Text("Реєстрація",weight=ft.FontWeight.BOLD, size=20)],
-                                        alignment=ft.MainAxisAlignment.CENTER,
-                                    ), 
-
-                                     ft.Row(
-                                        [ft.TextField(label="І'мя", prefix_icon=ft.icons.PERSON)],
-                                        alignment=ft.MainAxisAlignment.CENTER,
-                                    ), 
-                                       
-                                      ft.Row(
-                                        [ft.TextField(label="Email", prefix_icon=ft.icons.EMAIL)],
-                                        alignment=ft.MainAxisAlignment.CENTER,
-                                    ), 
-                              
-                                       ft.Row(
-                                        [ft.TextField(label="Пароль", password=True, can_reveal_password=True, prefix_icon=ft.icons.LOCK)],
-                                        alignment=ft.MainAxisAlignment.CENTER,
+                                    ft.Image(
+                                        f"https://github.com/VasinIgor96/New-Project/blob/master/Fonnn.jpg?raw=true",
+                                        fit=ft.ImageFit.FILL
                                     ),
-
-                                       ft.Row(
-                                        [ft.Checkbox(label="Я погоджуюсь з умовами обробки\n персональних данних", value=False, fill_color=ft.colors.BLUE_900)],
-                                        
-                                    ),
-
-                                    ft.Row(
-                                        [ft.ElevatedButton("Далі", width=120, style=ft.ButtonStyle (ft.colors.BLUE_900, side=ft.BorderSide(2, ft.colors.BLUE_900)), on_click=lambda _: page.go("/store"))],
-                                        alignment=ft.MainAxisAlignment.END,
-                                    ),
-                                ]
+                                ],
                             ),
-                           
-                        )
+                            ft.Container(
+                                margin=ft.margin.only(top=175),
+                                alignment=ft.alignment.center,
+                                content=ft.Card(
+                                    content=ft.Container(
+                                        height=400,
+                                        width=400,
+                                        padding=25,
+                                        content=ft.Column(
+                                            [
+
+                                                ft.Row(
+                                                    [
+                                                        ft.Text("Реєстрація", weight=ft.FontWeight.BOLD, size=20)
+                                                    ],
+                                                    alignment=ft.MainAxisAlignment.CENTER,
+                                                ),
+
+                                                ft.Row(
+                                                    [
+                                                        ft.TextField(label="І'мя", prefix_icon=ft.icons.PERSON)
+                                                    ],
+                                                    alignment=ft.MainAxisAlignment.CENTER,
+                                                ),
+
+                                                ft.Row(
+                                                    [
+                                                        ft.TextField(label="Email", prefix_icon=ft.icons.EMAIL)
+                                                    ],
+                                                    alignment=ft.MainAxisAlignment.CENTER,
+                                                ),
+
+                                                ft.Row(
+                                                    [
+                                                        ft.TextField(label="Пароль", password=True,can_reveal_password=True,prefix_icon=ft.icons.LOCK)
+                                                    ],
+                                                    alignment=ft.MainAxisAlignment.CENTER,
+                                                ),
+
+                                                ft.Row(
+                                                    [
+                                                        ft.Checkbox(label="Я погоджуюсь з умовами обробки \n персональних даних",value=False, fill_color=ft.colors.INDIGO_500)
+                                                    ],
+                                                ),
+
+                                                ft.Row(
+                                                    [
+                                                        ft.ElevatedButton("Далі", width=120,style=ft.ButtonStyle(ft.colors.BLUE_900,side=ft.BorderSide(2,ft.colors.INDIGO_500)),on_click=lambda _: page.go("/store"))
+                                                    ],
+                                                    alignment=ft.MainAxisAlignment.END,
+                                                ),
+                                            ]
+                                        ),
+                                    ),
+                                ),
+                            ),
+                        ]
                     ),
-                 ),
-             
-            ],
-        )
-                ],
+                ]
             )
-        )
+        ),
 
         if page.route == "/store":
             page.views.append(
                 ft.View(
                     "/store",
-                    [
+                  [
+                    ft.Stack(
+                        [
+                            ft.Row(
+                                [
+                                    ft.Image(
+                                       f"https://github.com/VasinIgor96/New-Project/blob/master/Fonnn.jpg?raw=true",
+                                       fit=ft.ImageFit.FILL
+                                    ),
+                                ],   
+                            ),
+                            ft.Row(
+                                    [
+                                       ft.IconButton(icon=ft.icons.OUTPUT_OUTLINED, width=80, style=ft.ButtonStyle(color={
+                                       ft.MaterialState.HOVERED: ft.colors.WHITE,
+                                       ft.MaterialState.DEFAULT: ft.colors.INDIGO_500,
+                                       }), icon_size=35, tooltip= "Вихід",  on_click=lambda _: page.go("/"))
+                                    ],
+                                        alignment=ft.MainAxisAlignment.END
+                                ),
+                            ft.Row(
 
-                        #ft.AppBar(title=ft.Text("Вихід"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.ElevatedButton("Розпочати тест", on_click=lambda _: page.go("/")),
-                    ],
-                    ft.Row(
-                        [ft.ElevatedButton("Вихід", width=120, style=ft.ButtonStyle (ft.colors.BLUE_900, side=ft.BorderSide(2, ft.colors.BLUE_900)))],
-                        alignment=ft.MainAxisAlignment.SPACE_AROUND
-                          )   
-                    ),
-                  
+                                    [
+                                       ft.ElevatedButton("Розпочати тест", width=600, style=ft.ButtonStyle(color={
+                                       ft.MaterialState.HOVERED: ft.colors.INDIGO_500,
+                                       ft.MaterialState.DEFAULT: ft.colors.BLACK,
+                                       }),)  
+                                    ],
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                    ),                       
+                            ],                   
+                        ),
+                    ]
                 ),
-                               
+            )
         page.update()
 
     def view_pop(view):
@@ -110,7 +140,6 @@ def main(page: ft.page):
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     page.go(page.route)
-
 
 
 
